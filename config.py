189fefca3e03af26d6705d1e587f5b3b4adb4da5
dmeyer4569 <dmeyer2@oakland.edu>
2025-07-03 14:18:57 -0400

    cleanup

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ EPOCHS = 5 # sets the amount of epochs ~ 11hrs/ea
 Z_DIM = 512  # Latent vector size
 BASE_CHANNELS = 512  # Base number of channels
 IMAGE_CHANNELS = 3  # Number of image channels (e.g., 3 for RGB)
-BATCH_SIZE = 16  # Default batch size
+BATCH_SIZE = 8  # Default batch size
 LEARNING_RATE = 0.0002  # Default learning rate
 
 #IMG H and W
